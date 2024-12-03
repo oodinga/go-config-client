1ecfccda87417a9cbf85e312bc507955afd44c7b
odinga96 <divadagnido96@gmail.com>
2024-12-04 00:59:44 +0300

    version 0.1.0

---
+++ configs/env.go
@@ -0,0 +1,112 @@
+// The config package defines functions for loading configs from a remote config server
+package configs
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// Config represents the configuration object
+
+type Config struct {
+	Name            string           `json:"name"`
+	Profiles        []string         `json:"profiles"`
+	Label           interface{}      `json:"label"`
+	Version         string           `json:"version"`
+	State           interface{}      `json:"state"`
+	PropertySources []PropertySource `json:"propertySources"`
+}
+
+type PropertySource struct {
+	Name   string                 `json:"name"`
+	Source map[string]interface{} `json:"source"`
+}
+
+type ConfigSettings struct {
+	Profiles  []string
+	ServerURL string
+	AppName   string
+	Optional  bool
+}
+
+var settings ConfigSettings
+
+func init() {
+	settings = ConfigSettings{
+		Profiles:  strings.Split(os.Getenv("app.config.profiles.active"), ","),
+		AppName:   os.Getenv("app.name"),
+		ServerURL: os.Getenv("app.config.server.url"),
+		Optional:  os.Getenv("app.config.optional") == "true",
+	}
+	log.Printf("Active profiles [%v]", strings.Join(settings.Profiles, ","))
+	loadConfigs()
+}
+
+// loadConfigs loops through the Profiles set in the environment variables and loads its configs for
+// the application. If an error occours, it is reported via #reportError(err error) function
+func loadConfigs() {
+	for _, profile := range settings.Profiles {
+		var err error
+		config, err := fetchConfigs(profile)
+
+		if err != nil {
+			reportError(err)
+			continue
+		}
+		setEnvVariables(config)
+	}
+}
+
+// Helper function used to check log errors that occurr during fetching of configs.
+// If the configs was set as optional, the errors will be excused, otherwise an exit code of 1 is executed
+// and the function is exited
+func reportError(err error) {
+	if !settings.Optional {
+		log.Fatal(err)
+	}
+
+	log.Print("Error fetching config:", err)
+}
+
+// Perharps the most critical fuction, it receives a config object, and uses the ,
+// source objects of the Soource objects values to set environment variables
+func setEnvVariables(config *Config) {
+	for _, property := range config.PropertySources {
+		for key, value := range property.Source {
+			os.Setenv(key, fmt.Sprint(value))
+		}
+	}
+}
+
+// fetchConfigs is a helper function used to fetch configs from a config server of a given profile
+// IT receives profile and returns a Config object or an error
+func fetchConfigs(profile string) (*Config, error) {
+	log.Printf("Fetching config from %s/%s/%s\n", settings.ServerURL, settings.AppName, profile)
+	resp, err := http.Get(fmt.Sprintf("%s/%s/%s", settings.ServerURL, settings.AppName, profile))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return UnmarshalConfig(body)
+}
+
+// Reds bytes and Unmarshals into Config object.
+// The service makes use of json.Unmarshal package.
+func UnmarshalConfig(data []byte) (*Config, error) {
+	var r Config
+	err := json.Unmarshal(data, &r)
+	return &r, err
+}
---
+++ configs/env_test.go
@@ -0,0 +1,42 @@
+package configs
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestUnmarshalConfig(t *testing.T) {
+	// Test Case 1: Valid JSON
+	validJSON := []byte(`{"name": "test", "port": 8080}`)
+
+	expectedConfig := &Config{
+		Name:     "",
+		Profiles: []string{"local,dev"},
+		Label:    "",
+		Version:  "",
+		PropertySources: []PropertySource{
+			{
+				Name: "",
+				Source: map[string]interface{}{
+					"server.port": "8080",
+				},
+			},
+		},
+	}
+
+	config, err := UnmarshalConfig(validJSON)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if !reflect.DeepEqual(config, expectedConfig) {
+		t.Errorf("expected %v, got %v", expectedConfig, config)
+	}
+
+	// Test Case 2: Invalid JSON
+	invalidJSON := []byte(`{"name": "test", "port": "invalid"}`) // invalid value for 'port'
+
+	_, err = UnmarshalConfig(invalidJSON)
+	if err == nil {
+		t.Errorf("expected an error, but got none")
+	}
+}
